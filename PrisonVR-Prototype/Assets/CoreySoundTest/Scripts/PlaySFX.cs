ce3fb029dc14ac36379c2e55bf6e947b0bd89745
8ude <crbertelsen@gmail.com>
2017-02-28 14:35:08 -0800

    Test Prison Sounds
    my folder cointains SteamVR and oculus audio plugins

---
+++ PrisonVR-Prototype/Assets/CoreySoundTest/Scripts/PlaySFX.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySFX : MonoBehaviour {
+
+	public AudioClip[] sfxClips;
+	AudioSource thisSource;
+
+	public float timeInterval;
+	public float timeJitter;
+
+	float coolDown;
+
+	public float pitchJitter;
+
+
+	// Use this for initialization
+	void Start () {
+
+		coolDown = 0;
+		thisSource = gameObject.GetComponent<AudioSource> ();
+		coolDown += Random.value * timeJitter - (0.5f * timeJitter);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		coolDown += Time.deltaTime;
+
+		if (coolDown >= timeInterval) {
+			thisSource.clip = sfxClips [Random.Range (0, sfxClips.Length)];
+			thisSource.pitch = 1.0f + (Random.value * pitchJitter);
+			thisSource.Play ();
+			coolDown = Random.value * timeJitter - (0.5f * timeJitter);
+		}
+			
+	}
+}
---
+++ PrisonVR-Prototype/Assets/CoreySoundTest/Scripts/TriggerMixerSnapshot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+public class TriggerMixerSnapshot : MonoBehaviour {
+
+	public AudioMixerSnapshot targetSnapshot;
+	public float fadeTime;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.name == "HeadCollider") { 
+			targetSnapshot.TransitionTo (fadeTime);
+		}
+	}
+}
