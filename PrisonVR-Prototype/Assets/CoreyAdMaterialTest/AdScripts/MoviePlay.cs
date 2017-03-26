23ae956a84afbf49233a816ded772118f528f5b8
8ude <crbertelsen@gmail.com>
2017-03-26 14:10:53 -0400

    AdMaterialTests

---
+++ PrisonVR-Prototype/Assets/CoreyAdMaterialTest/AdScripts/MoviePlay.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoviePlay : MonoBehaviour {
+	public MovieTexture movTexture;
+	void Start() {
+		GetComponent<Renderer>().material.mainTexture = movTexture;
+		movTexture.Play();
+	}
+}
