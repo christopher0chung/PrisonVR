1fc1fb88a2931da5ef25c3e83307fa6a77dbe6ad
8ude <crbertelsen@gmail.com>
2017-04-05 12:25:50 -0400

    Starting Ad Trigger

---
+++ PrisonVR-Prototype/Assets/CoreyAdMaterialTest/AdScripts/Advertisement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Advertisement: MonoBehaviour {
+	public MovieTexture movTexture;
+	public AudioClip movieAudio;
+
+	AudioSource movieSource;
+
+	void Start() {
+		
+		GetComponent<Renderer>().material.mainTexture = movTexture;
+		GetComponent<AudioSource> ();
+
+	}
+
+	public void PlayAd () { 
+
+		movTexture.Play();
+		movieSource.Play();
+
+	}
+}
