e71e4df1891be7196bfe90271a349b4a92f7c951
burgessv <bv534@nyu.edu>
2017-04-09 19:05:28 -0400

    progress on string parsing for book pages

---
+++ PrisonVR-Prototype/Assets/Scripts/Book/BookText.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class BookText : MonoBehaviour {
+
+	public string bookTextPath;
+
+	public int charsPerPage = 2000;
+
+	string textContents;
+	int currentIndex = 0;
+	string currentPageText;
+
+	public string NextPageText {
+		get {
+			SetNextPageText(); 
+			return currentPageText;
+		}
+	}
+
+	void Start () {
+		if (File.Exists(bookTextPath))
+		{
+			textContents = FileIOUtil.ReadStringFromFile(dataPath);
+			SetNextPageText();
+		}
+		else {
+			Debug.Log("file path is invalid. nothing done");
+		}
+	}
+	
+	void Update () {
+		
+	}
+
+	public void SetNextPageText () {
+		currentPageText = new string(textContents.ToCharArray(currentIndex, charsPerPage));
+	}
+}
