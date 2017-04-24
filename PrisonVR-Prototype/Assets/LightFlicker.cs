a08fd38b18a8c77c829830e341b2f5db90628aa6
Chris Chung <christopher0chung@gmail.com>
2017-04-24 17:53:39 -0400

    Backlight script.

---
+++ PrisonVR-Prototype/Assets/LightFlicker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour {
+
+    public bool screenOn;
+
+    Light mL;
+    float timer;
+	// Use this for initialization
+	void Start () {
+        mL = GetComponent<Light>();
+        screenOn = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (screenOn)
+        {
+            timer += Time.deltaTime;
+            if (timer >= .1f)
+            {
+                timer -= .1f;
+                mL.intensity = Random.Range(.5f, 1.1f);
+            }
+        }
+        else
+        {
+            mL.intensity = 0;
+        }
+	}
+}
