8dfcc81688fe4588a7a64a89ef60e23e2dee2ddd
Chris Chung <christopher0chung@gmail.com>
2017-05-08 16:28:53 -0400

    Tablet scripts.

---
+++ PrisonVR-Prototype/Assets/ObjectsTypeTag.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TypeTag { Hard, Soft, Wet }
+
+public class ObjectsTypeTag : MonoBehaviour {
+
+    public TypeTag thisCollidersTag;
+}
