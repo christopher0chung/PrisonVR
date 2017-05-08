ed725b002f8a610662626f5b9997828f4133ceba
Chris Chung <christopher0chung@gmail.com>
2017-05-08 13:06:05 -0400

    Gaze to screen and light color working.

---
+++ PrisonVR-Prototype/Assets/TurnLightRed.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnLightRed : MonoBehaviour {
+
+    [SerializeField] private Light myLight;
+    [SerializeField] LayerMask myLM;
+    private RaycastHit[] _myHits;
+    private Color startColor;
+
+    void Start()
+    {
+        startColor = myLight.color;
+    }
+
+	void Update () {
+        Debug.Log("in update");
+
+        Ray theRay = new Ray(transform.position, transform.forward);
+        Debug.DrawRay(theRay.origin, theRay.direction, Color.red, 100);
+        _myHits = Physics.RaycastAll(theRay, 100, myLM, QueryTriggerInteraction.Collide);
+
+        Debug.Log(_myHits.Length);
+
+        if (_myHits.Length > 0)
+            myLight.color = startColor;
+        else myLight.color = Color.red;
+	}
+}
