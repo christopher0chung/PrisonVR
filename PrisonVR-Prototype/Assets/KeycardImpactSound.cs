fc77f6ac997a6408e912e0e08f23da9d984a0025
8ude <crbertelsen@gmail.com>
2017-05-10 14:14:33 -0400

    keycard making sound

---
+++ PrisonVR-Prototype/Assets/KeycardImpactSound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeycardImpactSound : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision other) {
+		if (other.gameObject.name != "Mattress") {
+			PrisonAudioDirector.instance.Play3DSFX(
+				PrisonAudioDirector.instance.plexiImpact[Random.Range(0, PrisonAudioDirector.instance.plexiImpact.Length)],
+				transform.position,
+				0f
+				);
+		}
+	}
+}
